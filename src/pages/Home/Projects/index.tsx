8a55982a0f2aef044e6f024297f4eeab71f8f92e
FengDenny <fengdennyy@gmail.com>
2023-03-26 14:39:26 -0700

    Created Project section

---
+++ src/pages/Home/Projects/index.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import SectionLayout from "@/components/helper/SectionLayout";
+import Tabs from "@/components/tab/Tab";
+import StanceLab from "./StanceLab";
+import TheMovieBox from "./TheMovieBox";
+import WarframeLeeter from "./WarframeLeeter";
+import StanceLabLogo from "@/assets/project_logo/StanceLab.png";
+import TMBLogo from "@/assets/project_logo/TMB.png";
+import WFLLogo from "@/assets/project_logo/WFL.png";
+
+type Props = {};
+const tabs = [
+  {
+    id: 1,
+    image: StanceLabLogo, // define Label
+    Component: StanceLab, // assign Component
+  },
+  {
+    id: 2,
+    image: TMBLogo,
+    Component: TheMovieBox,
+  },
+  {
+    id: 3,
+    image: WFLLogo,
+    Component: WarframeLeeter,
+  },
+];
+const index = (props: Props) => {
+  const [selectedTab, setSelectedTab] = useState(tabs[0].id);
+
+  return (
+    <SectionLayout
+      id="projects"
+      styling={`relative flex  w-full flex-col gap-2   
+  `}
+      primaryColorBGStyle="bg-primary-300  -inset-8 rounded-tl-3-xl rounded-bl-3xl"
+    >
+      <h2 className="z-40 mx-auto flex w-4/6  justify-center pt-11 text-lg text-color-white underline decoration-color-description underline-offset-4 sm:justify-end sm:text-2xl">
+        Projects
+      </h2>
+      <Tabs
+        imageStyling="relative mx-auto h-80 pt-5  w-5/6 sm:w-8/12 py-5  rounded-md mt-5  z-40 flex 
+        flex-col"
+        primaryText="text-primary-100"
+        selectedTab={selectedTab}
+        onClick={setSelectedTab}
+        tabs={tabs}
+        onMouseEnter={setSelectedTab}
+      />
+    </SectionLayout>
+  );
+};
+
+export default index;
