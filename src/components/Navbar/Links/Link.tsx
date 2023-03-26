803e2707727ad06e9ab3cf151c3e00c153c3f977
FengDenny <fengdennyy@gmail.com>
2023-03-26 14:36:33 -0700

    Updated aLinkUnderline styling

--- src/components/Navbar/Links/Link.tsx
+++ src/components/Navbar/Links/Link.tsx
@@ -11,22 +11,20 @@ function Link({ page, selectedPage, setSelectedPage }: Props) {
   const toLoweredCase = page.toLowerCase().replace(" ", "") as SelectedPage;
 
   const aLinkUnderLine =
-    "border-b-0  bg-zeroThree  bg-0-100  bg-no-repeat transition-bgSize5sEaseInOut  hover:bg-OneHund3Pixel bg-0-100";
+    " bg-zeroThree  bg-0-100  bg-no-repeat transition-bgSize5sEaseInOut  hover:bg-OneHund3Pixel bg-gradient-lightPink";
 
   return (
     <a
       id={`#${toLoweredCase}`}
       href={`#${toLoweredCase}`}
-      className={` hover:text-primary-pink transition duration-200 ${
+      className={`transition duration-200 hover:text-primary-pink ${
         selectedPage === toLoweredCase ? `text-primary-pink` : ""
       } 
     
       `}
       onClick={() => setSelectedPage(toLoweredCase)}
     >
-      <span className={`${aLinkUnderLine} bg-gradient-lightPink  `}>
-        {page}
-      </span>
+      <span className={`${aLinkUnderLine} `}>{page}</span>
     </a>
   );
 }
