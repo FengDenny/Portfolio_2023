1d0a74ad8c1594cf7a9f6ab2800a5a4266ac97fa
FengDenny <fengdennyy@gmail.com>
2023-03-27 11:57:14 -0700

    Added starter file of ProjectInfoTemplate for each project

---
+++ src/pages/Home/Projects/StanceLab/ProjectInfo.tsx
@@ -0,0 +1,9 @@
+import ProjectInfoTemplate from "../ProjectInfoTemplate";
+
+type Props = {};
+
+const ProjectInfo = (props: Props) => {
+  return <ProjectInfoTemplate h2="StanceLab ProjectInfo" />;
+};
+
+export default ProjectInfo;
---
+++ src/pages/Home/Projects/TheMovieBox/ProjectInfo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ProjectInfoTemplate from "../ProjectInfoTemplate";
+type Props = {};
+
+const ProjectInfo = (props: Props) => {
+  return <ProjectInfoTemplate h2="TMB ProjectInfo" />;
+};
+
+export default ProjectInfo;
