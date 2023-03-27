610a7939be63e51ee5e5c67c0a7654ae5f4a6586
FengDenny <fengdennyy@gmail.com>
2023-03-27 11:07:10 -0700

    Changed a link to use HashLink to fix issues of not scrolling to homepage element

--- src/components/Navbar/Links/Link.tsx
+++ src/components/Navbar/Links/Link.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SelectedPage } from "@/Model/types";
+import { HashLink } from "react-router-hash-link";
 
 type Props = {
   page: string;
@@ -13,10 +14,11 @@ function Link({ page, selectedPage, setSelectedPage }: Props) {
   const aLinkUnderLine =
     " bg-zeroThree  bg-0-100  bg-no-repeat transition-bgSize5sEaseInOut  hover:bg-OneHund3Pixel bg-gradient-lightPink";
 
+  console.log(window.location.pathname === "/");
+
   return (
-    <a
-      id={`#${toLoweredCase}`}
-      href={`#${toLoweredCase}`}
+    <HashLink
+      to={`/#${toLoweredCase}`}
       className={`transition duration-200 hover:text-primary-pink ${
         selectedPage === toLoweredCase ? `text-primary-pink` : ""
       } 
@@ -25,7 +27,7 @@ function Link({ page, selectedPage, setSelectedPage }: Props) {
       onClick={() => setSelectedPage(toLoweredCase)}
     >
       <span className={`${aLinkUnderLine} `}>{page}</span>
-    </a>
+    </HashLink>
   );
 }
 
