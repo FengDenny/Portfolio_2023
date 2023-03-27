b42b5077d75b951b1a6a7254a5b5e1ad7bd06fb0
FengDenny <fengdennyy@gmail.com>
2023-03-27 11:56:50 -0700

    Initialized ProjectTemplate  for each project

--- src/pages/Home/Projects/StanceLab/StanceLab.tsx
+++ src/pages/Home/Projects/StanceLab/StanceLab.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import SLHero from "@/assets/project_image/stancelab_hero.png";
+import ProjectTemplate from "../ProjectTemplate";
 
 type Props = {};
 
 const StanceLab = (props: Props) => {
-  return <div>StanceLab</div>;
+  return (
+    <ProjectTemplate
+      image={SLHero}
+      alt="StanceLab"
+      spanH4="Stance Lab"
+      h4=", a single page
+    application, redesigned and developed by a team of two."
+      pageURL="/stancelab"
+    />
+  );
 };
 
 export default StanceLab;
--- src/pages/Home/Projects/TheMovieBox/TheMovieBox.tsx
+++ src/pages/Home/Projects/TheMovieBox/TheMovieBox.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import ProjectTemplate from "../ProjectTemplate";
+import TMBHero from "@/assets/project_image/tmb_hero.png";
 
 type Props = {};
 
 const TheMovieBox = (props: Props) => {
-  return <div>TheMovieBox</div>;
+  return (
+    <ProjectTemplate
+      image={TMBHero}
+      alt="TheMovvieBox"
+      spanH4="TheMovieBox"
+      h4=", a full stack 
+application, integrating TheMovieDatabase (TMDB) API and Firebase to display new or exisiting movies and shows."
+      pageURL="/themoviebox"
+    />
+  );
 };
 
 export default TheMovieBox;
