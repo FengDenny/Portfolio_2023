12c261c7f0141ec428e848ee33a360d76682c541
FengDenny <fengdennyy@gmail.com>
2023-03-27 11:51:22 -0700

    Updating styling for Project section

--- src/pages/Home/Projects/index.tsx
+++ src/pages/Home/Projects/index.tsx
@@ -3,7 +3,7 @@ import SectionLayout from "@/components/helper/SectionLayout";
 import Tabs from "@/components/tab/Tab";
 import StanceLab from "./StanceLab/StanceLab";
 import TheMovieBox from "./TheMovieBox/TheMovieBox";
-import WarframeLeeter from "./WarFrameLeeter/WarframeLeeter";
+import WarframeLeeter from "./WarframeLeeter/WarframeLeeter";
 import StanceLabLogo from "@/assets/project_logo/StanceLab.png";
 import TMBLogo from "@/assets/project_logo/TMB.png";
 import WFLLogo from "@/assets/project_logo/WFL.png";
@@ -12,8 +12,8 @@ type Props = {};
 const tabs = [
   {
     id: 1,
-    image: StanceLabLogo, // define Label
-    Component: StanceLab, // assign Component
+    image: StanceLabLogo,
+    Component: StanceLab,
   },
   {
     id: 2,
@@ -36,11 +36,11 @@ const index = (props: Props) => {
   `}
       primaryColorBGStyle="bg-primary-300  -inset-8 rounded-tl-3-xl rounded-bl-3xl"
     >
-      <h2 className="z-40 mx-auto flex w-4/6  justify-center pt-11 text-lg text-color-white underline decoration-color-description underline-offset-4 sm:justify-end sm:text-2xl">
+      <h2 className="z-40 mx-auto flex w-4/6  justify-center pt-24 text-lg text-color-white underline decoration-color-description underline-offset-4 sm:justify-end sm:text-2xl">
         Projects
       </h2>
       <Tabs
-        imageStyling="relative mx-auto h-80 pt-5  w-5/6 sm:w-8/12 py-5  rounded-md mt-5  z-40 flex 
+        imageStyling="relative mx-auto h-screen pt-5  w-5/6 sm:w-8/12 py-5  rounded-md mt-10  z-40 flex 
         flex-col"
         primaryText="text-primary-100"
         selectedTab={selectedTab}
