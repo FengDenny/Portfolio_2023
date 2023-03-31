29f1939ad949f4daacec0c82177395ec3decd23a
FengDenny <fengdennyy@gmail.com>
2023-03-30 20:34:13 -0700

    Created array of object data for StanceLab project images

---
+++ src/Model/image_snippets.ts
@@ -0,0 +1,20 @@
+import StanceLabContact from "@/assets/project_image/stancelab_contact.png";
+import StanceLabServices from "@/assets/project_image/stancelab_services.png";
+
+
+export const stanceLab_snippets: Array<{
+    id: number;
+    img_title: string;
+    img_snippet: string;
+  }> = [
+    {
+      id: 1,
+      img_title: "Contact",
+      img_snippet: StanceLabContact,
+    },
+    {
+      id: 2,
+      img_title: "Services",
+      img_snippet: StanceLabServices,
+    },
+  ];
