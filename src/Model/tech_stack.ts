cdbd69ca76f8b1181a2aa92699000578530a7ec0
FengDenny <fengdennyy@gmail.com>
2023-03-30 20:33:51 -0700

    Created array of object data for StanceLab project tech stack

---
+++ src/Model/tech_stack.ts
@@ -0,0 +1,44 @@
+import Bootstrap from "@/assets/logo_svg/tech_stack/bootstrap.svg";
+import JavaScript from "@/assets/logo_svg/tech_stack/JSLogo.svg";
+import PHP from "@/assets/logo_svg/tech_stack/php.svg";
+import HTML5 from "@/assets/logo_svg/tech_stack/html5.svg";
+import CSS3 from "@/assets/logo_svg/tech_stack/css3.svg";
+import MDB from "@/assets/logo_svg/tech_stack/mdb.svg";
+
+
+export const stanceLab_stack: Array<{
+    id: number;
+    stack_title: string;
+    stack_logo: string;
+  }> = [
+    {
+      id: 1,
+      stack_title: "Bootstrap",
+      stack_logo: Bootstrap,
+    },
+    {
+      id: 2,
+      stack_title: "JavaScript",
+      stack_logo: JavaScript,
+    },
+    {
+      id: 3,
+      stack_title: "PHP",
+      stack_logo: PHP,
+    },
+    {
+      id: 4,
+      stack_title: "HTML5",
+      stack_logo: HTML5,
+    },
+    {
+      id: 5,
+      stack_title: "CSS3",
+      stack_logo: CSS3,
+    },
+    {
+      id: 6,
+      stack_title: "MDB",
+      stack_logo: MDB,
+    },
+  ];
