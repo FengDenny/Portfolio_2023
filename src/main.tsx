1a07bde12e0168108812698c6da32787899b8f5e
FengDenny <fengdennyy@gmail.com>
2023-04-02 18:49:27 -0700

    Created scroll to top helper method to scroll back to the top after going to new page

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import ScrollToTop from "./components/helper/ScrollToTop";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </React.StrictMode>
