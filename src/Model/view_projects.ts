a0d78bf98dca589242774a429f014424ca5ec3db
FengDenny <fengdennyy@gmail.com>
2023-04-02 18:51:14 -0700

    Created nav links data for all projects in projects template

---
+++ src/Model/view_projects.ts
@@ -0,0 +1,17 @@
+export const view_projects = [
+    {
+      sectionID: "stancelab",
+      project: "Stance Lab",
+      to: "/stancelab",
+    },
+    {
+      sectionID: "themoviebox",
+      project: "TheMovieBox",
+      to: "/themoviebox",
+    },
+    {
+      sectionID: "warframeleeter",
+      project: "WarframeLeeter",
+      to: "/warframeleeter",
+    },
+  ]
