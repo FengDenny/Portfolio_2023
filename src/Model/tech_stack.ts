8aa059d0d41fbebe71bef464aa6929a1a15cb695
FengDenny <fengdennyy@gmail.com>
2023-04-02 18:50:38 -0700

    Added svg logos for warframeleeter/ themoviebox project tech stack

--- src/Model/tech_stack.ts
+++ src/Model/tech_stack.ts
@@ -4,6 +4,11 @@ import PHP from "@/assets/logo_svg/tech_stack/php.svg";
 import HTML5 from "@/assets/logo_svg/tech_stack/html5.svg";
 import CSS3 from "@/assets/logo_svg/tech_stack/css3.svg";
 import MDB from "@/assets/logo_svg/tech_stack/mdb.svg";
+import Firebase from "@/assets/logo_svg/firebase-icon.svg"
+import Sass from "@/assets/logo_svg/sass.svg"
+import Redux from "@/assets/logo_svg/redux.svg"
+import React from "@/assets/logo_svg/react.svg"
+import GraphQL from "@/assets/logo_svg/graphQL.svg"
 
 
 export const stanceLab_stack: Array<{
@@ -41,4 +46,71 @@ export const stanceLab_stack: Array<{
       stack_title: "MDB",
       stack_logo: MDB,
     },
+  ];
+export const tmb_stack: Array<{
+    id: number;
+    stack_title: string;
+    stack_logo: string;
+  }> = [
+    {
+      id: 1,
+      stack_title: "React",
+      stack_logo: React,
+    },
+    {
+      id: 2,
+      stack_title: "JavaScript",
+      stack_logo: JavaScript,
+    },
+    {
+      id: 3,
+      stack_title: "Redux",
+      stack_logo: Redux,
+    },
+    {
+      id: 4,
+      stack_title: "CSS3",
+      stack_logo: CSS3,
+    },
+    {
+      id: 5,
+      stack_title: "Firebase",
+      stack_logo: Firebase,
+    },
+    {
+      id: 6,
+      stack_title: "SASS",
+      stack_logo: Sass,
+    },
+  ];
+export const wfl_stack: Array<{
+    id: number;
+    stack_title: string;
+    stack_logo: string;
+  }> = [
+    {
+      id: 1,
+      stack_title: "React",
+      stack_logo: React,
+    },
+    {
+      id: 2,
+      stack_title: "JavaScript",
+      stack_logo: JavaScript,
+    },
+    {
+      id: 3,
+      stack_title: "Redux",
+      stack_logo: Redux,
+    },
+    {
+      id: 4,
+      stack_title: "SASS",
+      stack_logo: Sass,
+    },
+    {
+      id: 5,
+      stack_title: "GraphQL",
+      stack_logo: GraphQL,
+    }
   ];
