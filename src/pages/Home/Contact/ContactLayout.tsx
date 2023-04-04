1f2a38d55ecb11e6b614acacb67bb32b8ae65a4d
FengDenny <fengdennyy@gmail.com>
2023-04-04 11:29:48 -0700

    Forgot to create mobile responsiveness  first  so added now

--- src/pages/Home/Contact/ContactLayout.tsx
+++ src/pages/Home/Contact/ContactLayout.tsx
@@ -47,9 +47,16 @@ const ContactLayout = ({
   return (
     <section id="contact">
       <div className="mx-auto mt-40 flex w-4/6  flex-col  text-primary-100 dark:text-color-white">
-        <h2 className="mx-auto text-3xl font-bold ">Connect with me</h2>
+        <h2 className="mx-auto text-2xl  font-bold sm:text-3xl ">
+          Connect with me
+        </h2>
         <span className="mx-auto mt-1 w-44 border-b border-solid border-b-color-description"></span>
-        <p className="description-text relative left-6  mx-auto mt-4 w-80 text-xl font-normal text-color-description sm:w-96">
+        <p
+          className="description-text relative left-6  
+        mx-auto mt-4 w-64 text-xl 
+        font-normal text-color-description 
+        sm:w-96"
+        >
           Currently looking for opportunities to learn and grow. If any, send me
           an{" "}
           <span className="font-bold text-primary-300 dark:text-color-white">
@@ -70,7 +77,7 @@ const ContactLayout = ({
         </p>
       </div>
       <div className="mx-auto mt-2 flex h-auto  w-5/6 flex-col  flex-wrap justify-between py-24 text-color-white sm:flex-row">
-        <div className="relative top-6 mx-auto  h-auto w-72 md:w-80">
+        <div className="relative top-6 mx-auto  h-auto w-52 md:w-80">
           {theme ? (
             <img src={contact} alt="contact me" />
           ) : (
@@ -79,7 +86,10 @@ const ContactLayout = ({
         </div>
         <form
           ref={formRef}
-          className="relative top-4 left-4 mx-auto mt-10  flex h-auto w-96  flex-col rounded-lg bg-primary-300 p-10 shadow-lg shadow-primary-100 dark:bg-color-white  dark:shadow-primary-pink   sm:-top-6 sm:left-0 "
+          className="relative top-4 left-4 mx-auto mt-10  flex h-auto w-80  
+          flex-col rounded-lg bg-primary-300 p-10 shadow-lg shadow-primary-100 
+          dark:bg-color-white  dark:shadow-primary-pink   
+          sm:-top-6 sm:left-0 sm:w-96"
         >
           <h2 className="pb-6 text-color-white dark:text-primary-300">
             Send me an email
@@ -98,8 +108,9 @@ const ContactLayout = ({
               type="text"
               value={fullName}
               onChange={(e) => setFullName(e.target.value)}
-              className="description-text h-9 rounded-sm border-b  border-solid border-b-primary-300  px-1 text-lg font-normal text-primary-300 
-            caret-primary-pink-hover valid:focus-visible:outline-emerald-600 invalid:focus-visible:outline-rose-500"
+              className="description-text h-9 w-60 rounded-sm border-b  border-solid border-b-primary-300  
+              px-1 text-lg font-normal text-primary-300 caret-primary-pink-hover
+            valid:focus-visible:outline-emerald-600 invalid:focus-visible:outline-rose-500 sm:w-72 "
               required
               pattern="[a-zA-Z ]+"
             />
@@ -123,8 +134,9 @@ const ContactLayout = ({
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="description-text h-9 rounded-sm border-b  border-solid border-b-primary-300  px-1 text-lg font-normal text-primary-300 
-            caret-primary-pink-hover valid:focus-visible:outline-emerald-600 invalid:focus-visible:outline-rose-500"
+              className="description-text h-9 w-60 rounded-sm  border-b border-solid  
+              border-b-primary-300 px-1 text-lg font-normal  text-primary-300 caret-primary-pink-hover
+            valid:focus-visible:outline-emerald-600 invalid:focus-visible:outline-rose-500 sm:w-72"
               required
             />
             {emailError && (
@@ -143,20 +155,23 @@ const ContactLayout = ({
               name="message"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
-              className="description-text h-9 rounded-sm border-b  border-solid border-b-primary-300  px-1 text-lg font-normal text-primary-300 
-            caret-primary-pink-hover valid:focus-visible:outline-emerald-600 invalid:focus-visible:outline-rose-500"
+              className="description-text h-9 w-60 rounded-sm  border-b border-solid  
+              border-b-primary-300 px-1 text-lg font-normal text-primary-300 
+              caret-primary-pink-hover valid:focus-visible:outline-emerald-600 invalid:focus-visible:outline-rose-500 sm:w-72"
               required
             />
             {messageError && (
               <span className="mt-1 text-sm text-rose-500">{messageError}</span>
             )}
           </div>
-          <div className="mt-3">
+          <div className="mx-auto mt-3 sm:mx-0">
             {isLoading ? (
               <FaSpinner className="animate-spin text-2xl text-primary-pink" />
             ) : (
               <button
-                className="h-8 w-32 rounded-md border border-solid border-primary-pink bg-primary-pink text-color-description shadow-2xl"
+                className="h-8 w-32 rounded-md 
+                border border-solid border-primary-pink bg-primary-pink 
+                text-color-description shadow-2xl"
                 onClick={(e) => handleSend(e)}
                 disabled={
                   !enabled || !fullName || !email || !message || messageSent
