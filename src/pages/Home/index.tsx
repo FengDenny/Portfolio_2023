0717d2b1a5b5feedf6aa487333fa9d96331acf12
FengDenny <fengdennyy@gmail.com>
2023-04-04 12:42:16 -0700

    Added PageTitle to show HTML5 title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,18 +4,22 @@ import About from "./About";
 import TechStack from "./TechStack";
 import Project from "./Projects";
 import Contact from "./Contact/Contact";
+import PageTitle from "@/components/Helmet";
 
 type Props = {};
 
 function index({}: Props) {
   return (
-    <section id="home" className="gap-16 pt-60 md:h-full md:pb-0">
-      <Hero />
-      <About />
-      <TechStack />
-      <Project />
-      <Contact />
-    </section>
+    <>
+      <PageTitle pageTitle="Denny Feng | Welcome to my learning journey portfolio" />
+      <section id="home" className="gap-16 pt-60 md:h-full md:pb-0">
+        <Hero />
+        <About />
+        <TechStack />
+        <Project />
+        <Contact />
+      </section>
+    </>
   );
 }
 
