50a72ee612789e14f9b1f2ff49e6f084f4e54c0a
FengDenny <fengdennyy@gmail.com>
2023-04-03 17:48:55 -0700

    Added light/dark theme style / updated styling

--- src/pages/Home/TechStack/index.tsx
+++ src/pages/Home/TechStack/index.tsx
@@ -21,19 +21,24 @@ const tabs = [
 const index = (props: Props) => {
   const [selectedTab, setSelectedTab] = useState(tabs[0].id);
 
+  const darkThemeStyle = "dark:text-color-white";
+
+  const lightThemeStyle = "text-primary-100";
+
   return (
     <section
       id="stack"
       className={`relative mt-24 flex h-screen-70 w-full flex-col gap-2 sm:w-11.4/12 md:w-11.5/12 
 `}
     >
-      <h2 className="mx-auto flex w-4/6 justify-center underline decoration-color-description underline-offset-4 sm:text-2xl">
+      <h2
+        className={`${darkThemeStyle} ${lightThemeStyle} mx-auto flex w-4/6 justify-center underline decoration-color-description underline-offset-4 sm:text-2xl`}
+      >
         Tech Stack
       </h2>
       <AnimateOnScrollObserver styling="animate-popIn ">
         <Tabs
-          labelStyling="mx-auto h-auto pt-5 shadow-2xl w-5/6 sm:w-2/6 py-5 bg-color-white rounded-md mt-5 divide-y "
-          primaryText="text-primary-100"
+          labelStyling="mx-auto h-auto pt-5 shadow-2xl w-5/6 sm:w-2/6 py-5 dark:bg-color-white  bg-primary-100  rounded-md mt-5 divide-y "
           selectedTab={selectedTab}
           onClick={setSelectedTab}
           tabs={tabs}
