bda26c325cdeabb690d611f9871dbad0e94719e3
FengDenny <fengdennyy@gmail.com>
2023-04-03 17:49:44 -0700

    Added light/dark theme style / updated styling

--- src/pages/Home/Hero/index.tsx
+++ src/pages/Home/Hero/index.tsx
@@ -6,8 +6,12 @@ import AnimateOnScrollObserver from "@/components/helper/AnimateOnScrollObserver
 type Props = {};
 
 const index = (props: Props) => {
+  const darkThemeStyle = "dark:text-color-white";
+
+  const lightThemeStyle = "text-primary-100";
+
   const afterCardDesign =
-    "after:absolute after:top-0 after:block after:h-full after:w-full after:origin-top-left  after:skew-Y-17 after:bg-primary-300";
+    "after:absolute after:top-0 after:block after:h-full after:w-full after:origin-top-left  after:skew-Y-17 after:bg-primary-100 after:dark:bg-color-white";
 
   const smallViewPortCardDesign = "sm:w-3/6 md:w-4/6 sm:top-2  sm:right-0";
 
@@ -24,19 +28,21 @@ const index = (props: Props) => {
         <div className="flex grow basis-4/5 flex-col">
           <AnimateOnScrollObserver styling="animate-inRight ">
             <h2
-              className="
-          w-64 text-2xl  sm:w-80 sm:text-3xl md:w-10/12 
-          md:text-4xl md:leading-normal
-        "
+              className={`${darkThemeStyle} ${lightThemeStyle} w-64 text-2xl  sm:w-80 sm:text-3xl md:w-10/12 
+              md:text-4xl md:leading-normal`}
             >
               Learning and building anything to gain hands-on experience
-              <span className="text-base text-primary-yellow">.</span>
+              <span className="text-lg text-primary-pink-hover dark:text-primary-yellow">
+                .
+              </span>
             </h2>
           </AnimateOnScrollObserver>
           <AnimateOnScrollObserver styling="animate-inLeft">
             <span className="description-text  pt-2 text-base  text-color-description sm:text-lg">
               Computer rigs, websites, frameworks, anything really
-              <span className="text-base text-primary-yellow">..</span>
+              <span className="text-lg text-primary-pink-hover dark:text-primary-yellow">
+                ..
+              </span>
             </span>
           </AnimateOnScrollObserver>
           <ul className="flex flex-row pt-4 text-3xl">
@@ -69,7 +75,7 @@ const index = (props: Props) => {
         <div
           className={`relative top-5 right-4 h-64 w-full  origin-bottom
         -skew-x-6  animate-popIn overflow-hidden rounded-xl
-        bg-primary-300 shadow-2xl
+         shadow-2xl
         ${smallViewPortCardDesign} ${afterCardDesign}`}
         >
           <img
