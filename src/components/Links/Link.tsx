557e302e4919cf90539e56bec03b1ef77b5cf611
FengDenny <fengdennyy@gmail.com>
2023-04-04 14:41:14 -0700

    Added dark/light theme coloring for links

--- src/components/Links/Link.tsx
+++ src/components/Links/Link.tsx
@@ -13,13 +13,16 @@ function Link({ page, selectedPage, setSelectedPage, styling }: Props) {
   const toLoweredCase = page.toLowerCase().replace(" ", "") as SelectedPage;
 
   const aLinkUnderLine =
-    " bg-zeroThree  bg-0-100  bg-no-repeat transition-bgSize5sEaseInOut  hover:bg-OneHund3Pixel bg-gradient-lightPink";
+    " bg-zeroThree  bg-0-100  bg-no-repeat transition-bgSize5sEaseInOut  hover:bg-OneHund3Pixel bg-gradient-lightPink ";
 
   return (
     <HashLink
       to={`/#${toLoweredCase}`}
-      className={`transition duration-200 hover:text-primary-pink ${styling} ${
-        selectedPage === toLoweredCase ? `text-primary-pink` : ""
+      className={`text-lg text-color-description transition
+      duration-200 hover:text-primary-pink dark:text-color-white dark:hover:text-primary-pink ${styling} ${
+        selectedPage === toLoweredCase
+          ? `text-primary-pink dark:text-primary-pink`
+          : ""
       } 
     
       `}
