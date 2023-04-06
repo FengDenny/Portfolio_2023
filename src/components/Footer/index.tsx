3f2262b2a01d308b89f0e77c788a9f75efc5245a
FengDenny <fengdennyy@gmail.com>
2023-04-06 14:51:53 -0700

    Added mobile/desktop styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -21,8 +21,8 @@ const index = ({ selectedPage, setSelectedPage }: Props) => {
     "transtion delay-75 duration-100 ease-in hover:scale-110 hover:text-primary-pink-hover";
 
   return (
-    <footer className="bg-color-white py-24   transition-colors  ease-in-out dark:bg-primary-300">
-      <div className="mx-auto flex  w-4/6  flex-col justify-between sm:flex-row">
+    <footer className="bg-color-white py-20   transition-colors  ease-in-out dark:bg-primary-300">
+      <div className="mx-auto flex  w-4/6  flex-col justify-between md:flex-row">
         <div className="mx-auto sm:mx-0">
           {theme ? (
             <img
@@ -37,15 +37,15 @@ const index = ({ selectedPage, setSelectedPage }: Props) => {
               className="h-36 w-36 rotate-45 sm:h-28 sm:w-28"
             />
           )}
-          <h4 className="description-text  text-xl font-bold text-primary-300 dark:text-color-white">
+          <h4 className="description-text  relative text-xl text-primary-300 dark:text-color-white md:top-12">
             © 2023 Denny Feng.
           </h4>
         </div>
         <div
-          className={`relative top-12  mx-auto grid grid-cols-3  gap-4
-          text-sm text-primary-100
-          dark:text-color-white below397:grid-cols-1
-          sm:mx-0 sm:grid-cols-2 sm:gap-x-4`}
+          className={`relative top-12 mx-auto  grid grid-cols-3 gap-4  text-sm
+          text-primary-100 dark:text-color-white
+          below397:grid-cols-1 sm:mx-0
+          sm:gap-x-4 md:top-14 md:grid-cols-2`}
         >
           {navLinks.map((items: any) => {
             const { title } = items;
@@ -61,27 +61,34 @@ const index = ({ selectedPage, setSelectedPage }: Props) => {
             );
           })}
         </div>
-        <div className="relative top-16 sm:top-12">
-          <div className="flex flex-row justify-center py-4  below397:flex-col">
+        <div className="relative top-12 below768:mx-auto xs:top-12  selection:md:mx-0">
+          <div className="relative flex flex-row below397:flex-col below768:top-4 sm:pt-4 md:top-0 md:pt-0 ">
             {theme ? (
               <img
                 src={emailDark}
                 alt="email"
-                className="bewlow397:w-32 mx-auto sm:w-20"
+                className="mx-auto w-24 below397:w-32 sm:w-20"
               />
             ) : (
               <img
                 src={email}
                 alt="email"
-                className="bewlow397:w-32 mx-auto sm:w-20"
+                className="w-24 below397:w-32 xs:mx-auto sm:w-20"
               />
             )}
-            <span className="description-text text-color-primary-100 relative top-1  px-2 dark:text-color-white sm:top-8">
+            <span
+              className="description-text text-color-primary-100 relative top-8  px-2  dark:text-color-white xs:mx-auto sm:top-8
+            
+            "
+            >
               fengdennyy@gmail.com
             </span>
           </div>
 
-          <ul className="flex flex-row justify-center pt-4 text-3xl sm:justify-between">
+          <ul
+            className="flex flex-row justify-center pt-10 text-3xl 
+          sm:pl-8 md:justify-between"
+          >
             <li>
               <a
                 href="https://github.com/FengDenny"
