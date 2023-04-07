3327686e43553ee6be43401a37c21174209fa390
FengDenny <fengdennyy@gmail.com>
2023-04-06 21:56:10 -0700

    Added base URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"https://dennyfeng.com",
   plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
